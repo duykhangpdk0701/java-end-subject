506c31329ba46a99c8faead43fe16eb92a7cca99
Phùng Duy Khang <61218936+duykhangpdk0701@users.noreply.github.com>
2021-09-09 10:26:01 +0700

    add edit category

--- src/GUI/CategoryGUI.java
+++ src/GUI/CategoryGUI.java
@@ -52,6 +52,14 @@ public class CategoryGUI extends JPanel {
         loadCategory();
     }
 
+    private void editBtnActionPerformed(ActionEvent e) {
+        DefaultTableModel dtm = (DefaultTableModel) table1.getModel();
+        int value = Integer.parseInt(dtm.getValueAt(table1.getSelectedRow(), 0).toString());
+        EditCategoryGUI editCategoryGUI = new EditCategoryGUI(value);
+        editCategoryGUI.setVisible(true);
+        loadCategory();
+    }
+
 
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
@@ -62,16 +70,16 @@ public class CategoryGUI extends JPanel {
         editBtn = new JButton();
 
         //======== this ========
-        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border
-                .EmptyBorder(0, 0, 0, 0), "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn", javax.swing.border.TitledBorder.CENTER, javax
-                .swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dia\u006cog", java.awt.Font.BOLD,
-                12), java.awt.Color.red), getBorder()));
-        addPropertyChangeListener(new java.beans
-                .PropertyChangeListener() {
+        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder
+                (0, 0, 0, 0), "JF\u006frmDes\u0069gner \u0045valua\u0074ion", javax.swing.border.TitledBorder.CENTER, javax.swing.border
+                .TitledBorder.BOTTOM, new java.awt.Font("D\u0069alog", java.awt.Font.BOLD, 12), java.awt
+                .Color.red), getBorder()));
+        addPropertyChangeListener(new java.beans.PropertyChangeListener() {
             @Override
-            public void propertyChange(java.beans.PropertyChangeEvent e) {
-                if ("\u0062ord\u0065r".equals(e.
-                        getPropertyName())) throw new RuntimeException();
+            public void
+            propertyChange(java.beans.PropertyChangeEvent e) {
+                if ("\u0062order".equals(e.getPropertyName())) throw new RuntimeException()
+                        ;
             }
         });
         setLayout(null);
@@ -108,6 +116,7 @@ public class CategoryGUI extends JPanel {
 
         //---- editBtn ----
         editBtn.setText("S\u1eeda");
+        editBtn.addActionListener(e -> editBtnActionPerformed(e));
         add(editBtn);
         editBtn.setBounds(865, 515, 100, 40);
 
---
+++ src/GUI/EditCategoryGUI.java
@@ -0,0 +1,117 @@
+/*
+ * Created by JFormDesigner on Thu Sep 09 09:57:16 ICT 2021
+ */
+
+package GUI;
+
+import BLL.CategoryBLL;
+import DTO.CategoryDTO;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * @author unknown
+ */
+public class EditCategoryGUI extends JFrame {
+    CategoryBLL categoryBLL = new CategoryBLL();
+
+    public EditCategoryGUI(int id) {
+        initComponents();
+        loadCategory(id);
+    }
+
+    public void loadCategory(int id) {
+        CategoryDTO categoryDTO = categoryBLL.getCategoryById(id);
+        nameInput.setText(categoryDTO.getName());
+        descriptionInput.setText(categoryDTO.getDescription());
+    }
+
+
+    private void confirmBtnActionPerformed(ActionEvent e) {
+        if (nameInput.getText().trim().equals("") || descriptionInput.getText().trim().equals("")) {
+            JOptionPane.showMessageDialog(this, "Vui lòng nhập đầy đủ thông tin");
+        } else {
+
+        }
+    }
+
+    private void confimBtnActionPerformed(ActionEvent e) {
+        // TODO add your code here
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - unknown
+        label1 = new JLabel();
+        nameLabel = new JLabel();
+        descriptionLabel = new JLabel();
+        nameInput = new JTextField();
+        descriptionInput = new JTextField();
+        confirmBtn = new JButton();
+
+        //======== this ========
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        var contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        //---- label1 ----
+        label1.setText("S\u1eeda m\u1ee5c");
+        label1.setHorizontalAlignment(SwingConstants.CENTER);
+        label1.setFont(label1.getFont().deriveFont(label1.getFont().getSize() + 10f));
+        contentPane.add(label1);
+        label1.setBounds(105, 65, 375, 40);
+
+        //---- nameLabel ----
+        nameLabel.setText("Name:");
+        contentPane.add(nameLabel);
+        nameLabel.setBounds(95, 145, 80, 30);
+
+        //---- descriptionLabel ----
+        descriptionLabel.setText("Description:");
+        contentPane.add(descriptionLabel);
+        descriptionLabel.setBounds(95, 225, 80, 30);
+        contentPane.add(nameInput);
+        nameInput.setBounds(185, 145, 295, 30);
+        contentPane.add(descriptionInput);
+        descriptionInput.setBounds(185, 225, 295, 30);
+
+        //---- confirmBtn ----
+        confirmBtn.setText("X\u00e1c nh\u1eadn");
+        confirmBtn.addActionListener(e -> {
+            confimBtnActionPerformed(e);
+            confirmBtnActionPerformed(e);
+        });
+        contentPane.add(confirmBtn);
+        confirmBtn.setBounds(445, 305, 100, 40);
+
+        {
+            // compute preferred size
+            Dimension preferredSize = new Dimension();
+            for (int i = 0; i < contentPane.getComponentCount(); i++) {
+                Rectangle bounds = contentPane.getComponent(i).getBounds();
+                preferredSize.width = Math.max(bounds.x + bounds.width, preferredSize.width);
+                preferredSize.height = Math.max(bounds.y + bounds.height, preferredSize.height);
+            }
+            Insets insets = contentPane.getInsets();
+            preferredSize.width += insets.right;
+            preferredSize.height += insets.bottom;
+            contentPane.setMinimumSize(preferredSize);
+            contentPane.setPreferredSize(preferredSize);
+        }
+        setSize(595, 415);
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - unknown
+    private JLabel label1;
+    private JLabel nameLabel;
+    private JLabel descriptionLabel;
+    private JTextField nameInput;
+    private JTextField descriptionInput;
+    private JButton confirmBtn;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
